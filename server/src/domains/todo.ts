0912af66a16127faf92391103dea577370ce5e1e
teru12012000 <sion2769@gmail.com>
2024-01-06 20:53:34 +0900

    feat: add domains of todo

---
+++ server/src/domains/todo.ts
@@ -0,0 +1,56 @@
+import { ID } from "../types/todo"
+/*
+ここではdomainを実装している。
+domainsはルールの手続きみたいなもの。
+今回はtodoなのでtodoのルールを定義している
+
+例えば
+今回はユーザからtodoの内容(content)を受け取ることになっている。
+コンテントは空文字だとおかしなことになってしまう。
+フロントエンドであらかじめバリデーションをチェックすることもできるが
+今回はここでチェックする
+*/
+export class Todo {
+    private _id: ID
+    private _content: string
+    private _checked: number
+
+    get id(): ID {
+        return this._id
+    }
+
+    set id(id: ID) {
+        this._id = id
+    }
+
+    get content(): string {
+        return this._content
+    }
+
+    set content(content: string) {
+        this._content = content
+    }
+
+    get isChecked(): number {
+        return this._checked
+    }
+
+    set isChecked(isChecked: number) {
+        this._checked = isChecked
+    }
+
+    //ここではコンテンツが空だとfalseを返す
+    isContentFilled(): boolean {
+        return this._content.length > 0
+    }
+
+    //初期化
+    constructor(content: string) {
+        this._content = content
+        this._checked = 0
+    }
+}
+
+/*
+getとsetについては後にまとめる
+*/
